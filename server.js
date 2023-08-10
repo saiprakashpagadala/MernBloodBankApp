8c50952ed1f10d79953c56ed92b0cbeaacfe514a
saiprakash <saiprakash.pagadala@scriptbees.com>
2023-08-10 17:47:47 +0530

    test server created

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+// rest object
+const app = express();
+
+// routes
+
+// test route
+app.get("/test", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to Blood Bank Application",
+  });
+});
+
+// port
+const PORT = 8080;
+
+// listen
+app.listen(PORT, () => {
+  console.log(`Node Server Running at port ${PORT}`);
+});
