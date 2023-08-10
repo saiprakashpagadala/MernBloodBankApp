4c54af3634207512926f28a7ddf4176acdbaa9b7
saiprakash <saiprakash.pagadala@scriptbees.com>
2023-08-10 18:47:25 +0530

    needed packages installed

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-//imports
-import express from "express";
-import testRoutes from './routes/testRoute.js'
+// package imports
+import express, { json } from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import cors from "cors";
+import morgan from "morgan";
+
+// file imports
+import testRoutes from "./routes/testRoute.js";
+
+// dotenv config
+dotenv.config();
 
 // rest object
 const app = express();
 
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
 // routes
-app.use("/api/v1/test", testRoutes)
+app.use("/api/v1/test", testRoutes);
 
 // port
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+const MODE = process.env.DEV_MODE;
 
 // listen
 app.listen(PORT, () => {
-  console.log(`Node Server Running at port ${PORT}`);
+  console.log(`Node Server Running at port ${PORT} in ${MODE}`.bgBlue.white);
 });
