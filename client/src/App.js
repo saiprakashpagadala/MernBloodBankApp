6ee4cb8ef2971e44eea46073b295613d05023a62
saiprakash <saiprakash.pagadala@scriptbees.com>
2023-08-14 13:25:38 +0530

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 
 function App() {
   return (
-    <div>
-      <h1>Blood Bank Application</h1>
-    </div>
+    <>
+      <div>
+        <h1 className="text-center text-info mt-3">Blood Bank Application</h1>
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
