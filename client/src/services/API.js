9bfb01b41359a3edb9a6f279d60e885194a0673f
saiprakash <saiprakash.pagadala@scriptbees.com>
2023-08-16 15:36:35 +0530

    redux toolkit, concurrently added

---
+++ client/src/services/API.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: process.env.REACTAPP_BASE_URL,
+});
+
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("token")) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  }
+  return req;
+});
+
+export default API;
